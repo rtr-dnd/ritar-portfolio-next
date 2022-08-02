2bda91a261999e24269f4a5c72fc8a61f8c3f711
rtr-dnd <skysoyn@gmail.com>
2022-08-02 23:25:35 +0900

    :sparkles: blend added

---
+++ pages/blend.tsx
@@ -0,0 +1,118 @@
+import Image from 'next/future/image'
+import { NextPage } from "next"
+import styles from '../styles/blend.module.css'
+import { useEffect, useRef, useState, WheelEventHandler } from 'react'
+import Head from 'next/head'
+
+const getWindowDimensions = () => {
+  if (typeof window == 'undefined') return
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  }
+}
+
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+
+const Content = () => {
+  return (
+    <div className={styles.contentwrap}>
+      <Image className={styles.image} src="/blend/blend_1.jpg" alt="bg" priority/>
+      <h1 className={styles.title}>
+        <span>HONGO</span>
+        <span>DESIGN</span>
+        <span>DAY</span>
+      </h1>
+      <Image className={styles.image} src="/blend/blend_2.jpg" alt="bg" priority/>
+      <Image className={styles.image} src="/blend/blend_3.jpg" alt="bg" priority/>
+      <Image className={styles.image} src="/blend/blend_4.jpg" alt="bg" priority/>
+      <Image className={styles.image} src="/blend/blend_5.jpg" alt="bg" priority/>
+      <div className={styles.desc}>
+        <span>at <a className={styles.link} href='https://lab-cafe.net/'>LabCafe</a></span>
+        <span>from 7:30 pm on every tuesday</span>
+      </div>
+      <Image className={styles.image} src="/blend/blend_6.jpg" alt="bg" priority/>
+      <Image className={styles.image} src="/blend/blend_7.jpg" alt="bg" priority/>
+      <Image className={styles.image} src="/blend/blend_8.jpg" alt="bg" priority/>
+      <div className={styles.foot}>
+        <a className={styles.link} href='http://designingplusnine.com/'>
+          <Image className={styles.footlogo} src="/dp9.svg" alt="bg" priority/>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+const Blend: NextPage = () => {
+  const scrollGain = 1
+  const [scrollTop, setScrollTop] = useState(0)
+  const ref = useRef<HTMLDivElement>(null)
+  const contentRef = useRef<HTMLDivElement>(null)
+  const dimensions = useWindowDimensions()
+  const {height, width} = dimensions ? dimensions : {width: 0, height: 0}
+  const [header, setHeader] = useState<string>('SCROLL DOWN')
+
+  const onWheel = (e: WheelEvent) => {
+    e.preventDefault()
+    const val = scrollTop + e.deltaY * scrollGain
+    if (val < 0) return
+    if (!contentRef.current?.clientHeight) return
+    if (val > contentRef.current?.clientHeight - height) return
+    if (val < 5) {
+      setHeader('SCROLL DOWN')
+    } else {
+      setHeader('HONGO DESIGN DAY')
+    }
+    setScrollTop(val)
+  }
+
+  useEffect(() => {
+    ref.current?.addEventListener("wheel", onWheel, {passive: false})
+    return (() => {
+      ref.current?.removeEventListener("wheel", onWheel)
+    })
+  })
+
+  return (
+    <div className={styles.container} ref={ref}>
+      <Head>
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"></meta>
+      </Head>
+      {/* <Content isFlipped={false}/>
+      <Content isFlipped={true}/> */}
+      <div className={styles.fixedcontent}>
+        <div className={styles.logo}>{header}</div>
+      </div>
+      <div className={styles.flippedfixedcontent}>
+        <div className={styles.logo}>{header}</div>
+      </div>
+      <div className={styles.content} ref={contentRef} style={{
+        marginTop: scrollTop * -1 + 'px'
+      }}>
+        <Content />
+      </div>
+      <div className={styles.content + ' ' + styles.flipped} style={{
+        bottom: scrollTop * -1 + 'px'
+      }}>
+        <Content />
+      </div>
+    </div>
+  )
+}
+
+export default Blend
