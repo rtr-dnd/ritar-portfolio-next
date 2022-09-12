12c241fe2d7491740a6cc0add062d45f66b001cf
rtr-dnd <skysoyn@gmail.com>
2022-09-12 10:06:29 +0900

    :chart_with_upwards_trend: ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,22 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../store'
 
+const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ''
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}></script>
+      <script dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){(dataLayer).push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${GA_ID}');`
+      }}
+      />
+        
     </Provider>
   )
 }
